910ffbee4cd66839849fc70a5ff3f52704cb7bac
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-07-18 15:34:50 -0300

    Cria getters e setters

--- src/domain/Product.java
+++ src/domain/Product.java
@@ -13,5 +13,35 @@ public class Product {
         this.quantity = quantity;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
