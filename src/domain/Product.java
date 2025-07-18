2e6011d425afac71c2a4c73f93943b5f237bbf14
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2025-07-18 15:33:11 -0300

    Cria construtor de produto

--- src/domain/Product.java
+++ src/domain/Product.java
@@ -5,4 +5,13 @@ public class Product {
     private String description;
     private double price;
     private int quantity;
+
+    public Product(String name, String description, double price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
 }
